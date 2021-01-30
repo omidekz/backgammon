e86b00697097c091ab64526ab1f099d4b06af61b
omidekz <omidekz@gmail.com>
2021-01-30 20:26:43 +0330

    make str repr

--- models/marble.py
+++ models/marble.py
@@ -4,3 +4,9 @@ from enum import Enum
 class Marble(str, Enum):
     BLACK = 'b'
     WHITE = 'w'
+
+    def __str__(self):
+        return self.value
+
+    def __repr__(self):
+        return str(self)
