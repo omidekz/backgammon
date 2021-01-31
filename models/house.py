7200683756dd9dbbd63b5450496826d05b2e497c
omidekz <omidekz@gmail.com>
2021-02-01 00:43:36 +0330

    refactor code style

--- models/house.py
+++ models/house.py
@@ -21,6 +21,11 @@ class House:
         self.house_number = house_number
 
     @staticmethod
+    def build(house_number: int) -> House:
+        host, number = House.create_house_detail(house_number)
+        return House([host] * number, house_number)
+
+    @staticmethod
     def create_house_detail(house_number: int) -> Tuple[Marble, int]:
         if house_number not in House.MARBLES_HOUSE_NUMBERS:
             return (None, 0)
@@ -29,11 +34,16 @@ class House:
         elif 8 <= house_number <= 17:
             return (House.MARBLES_HOUSE_TYPE[house_number], 3)
         return (House.MARBLES_HOUSE_TYPE[house_number], 2)
- 
-    @staticmethod
-    def build(house_number: int) -> House:
-        host, number = House.create_house_detail(house_number)
-        return House([host] * number, house_number)
+
+    def host(self) -> Optional[Marble]:
+        return self.host_continual() or self.host_premitive()
+
+    def is_host(self, marble) -> bool:
+        host = self.host()
+        return host and host == marble
+
+    def has_host(self) -> bool:
+        return bool(self.host())
 
     def marble_counter(self, marble: Marble) -> int:
         return self.marbles.count(marble)
@@ -41,16 +51,16 @@ class House:
     def marbles_counter(self, marbles: Sequence[Marble] = [Marble.BLACK, Marble.WHITE]) -> Sequence[Marble, int]:
         return list(map(self.marble_counter, marbles))
 
-    def host_premitive(self) -> Optional[Marble]:
-        blacks, whites = self.marbles_counter()
-        return self.premitive_conditions(blacks, whites)
-
     def premitive_conditions(self, blacks: int = 0, whites: int = 0) -> Optional[Marble]:
         if blacks == 0 and whites == 0:
             return None
         elif whites > 0:
             return Marble.WHITE
         return Marble.BLACK
+
+    def host_premitive(self) -> Optional[Marble]:
+        blacks, whites = self.marbles_counter()
+        return self.premitive_conditions(blacks, whites)
 
     def continual_conditions(self, blacks: int = 0, whites: int = 0) -> Optional[Marble]:
         if blacks < 2 and whites < 2:
@@ -62,17 +72,6 @@ class House:
     def host_continual(self) -> Optional[Marble]:
         blacks, whites = self.marbles_counter()
         return self.continual_conditions(blacks, whites)
-
-
-    def host(self) -> Optional[Marble]:
-        return self.host_continual() or self.host_premitive()
-
-    def is_host(self, marble) -> bool:
-        host = self.host()
-        return host and host == marble
-
-    def has_host(self):
-        return bool(self.host())
 
     def is_continual_host(self, marble: Marble) -> bool:
         return marble == self.host_continual()
