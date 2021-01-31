c93a060ecba7da6b0128df0f58a8058d32ac0b3c
omidekz <omidekz@gmail.com>
2021-01-31 21:46:55 +0330

    create board_items method and make more readable marble_house method, create get_house, can_move_marble_to, move_marble methods

--- models/board.py
+++ models/board.py
@@ -22,12 +22,25 @@ class Board:
     def next(self, toss: Sequence[int]) -> None:
         self.current_turn_toss = toss or Dice.toss()
 
+    def board_items(self) -> Sequence[int, House]:
+        return self.board.items()
+
     def marble_house(self, marble: Marble, indexs_seq=False) -> Sequence[Union[int, House]]:
         result_sequence = []
-        for house_number, house in self.board.items():
-            if house.host() == marble:
+        for house_number, house in self.board_items():
+            if house.is_host(marble):
                 result_sequence.append(house_number if indexs_seq else house)
         return result_sequence
+
+    def get_house(self, house: Union[House, int]) -> House:
+        return house if isinstance(house, House) else self[house]
+
+    def can_move_marble_to(self, marble: Marble, house: Union[House, int]) -> bool:
+        house = self.get_house(house)
+        return house.can_add(marble)
+
+    def move_marble(self, src: Union[House, int], dst: Union[House, int], marble: Marble) -> bool:
+        return src.is_host(marble) and self.can_move_marble_to(marble, dst)
 
     def __getitem__(self, house_number: int) -> House:
         if not 1 <= house_number <= 24:
