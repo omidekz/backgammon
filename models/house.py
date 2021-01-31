296b53316216bc0e73bea1ef43d45858d73f8ff5
omidekz <omidekz@gmail.com>
2021-02-01 00:46:11 +0330

    fix a bad return type

--- models/house.py
+++ models/house.py
@@ -48,7 +48,7 @@ class House:
     def marble_counter(self, marble: Marble) -> int:
         return self.marbles.count(marble)
 
-    def marbles_counter(self, marbles: Sequence[Marble] = [Marble.BLACK, Marble.WHITE]) -> Sequence[Marble, int]:
+    def marbles_counter(self, marbles: Sequence[Marble] = [Marble.BLACK, Marble.WHITE]) -> Sequence[int]:
         return list(map(self.marble_counter, marbles))
 
     def premitive_conditions(self, blacks: int = 0, whites: int = 0) -> Optional[Marble]:
