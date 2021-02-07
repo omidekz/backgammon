0b3689dd547ba267de7079341e5133122fee2d31
omidekz <omidekz@gmail.com>
2021-02-08 00:06:45 +0330

    some changes

--- models/marble.py
+++ models/marble.py
@@ -11,10 +11,13 @@ class Marble(str, Enum):
         return self == Marble.BLACK and distance > 0 \
                 or self == Marble.WHITE and distance < 0
 
+    def __bool__(self):
+        return True
+
     def get_destination(self, src: int, moves: int) -> int:
         if self == Marble.BLACK:
-            src + moves
-        return abs(src - moves) + 1
+            moves *= -1
+        return src + moves
 
     def __str__(self):
         return self.value
