a3d9f1b53b96aedb5f4720aee4906146cdf61282
omidekz <omidekz@gmail.com>
2021-02-08 09:29:07 +0330

    fix algos init

--- algorithms/__init__.py
+++ algorithms/__init__.py
@@ -1,5 +1,9 @@
-from .board import create_boards
+from .boardstates import create_boards
+from .boardutils import get_board_section, Section as BoardSection
+from .house import house_collection_to_string
 
 __all__ = [
-    'create_boards'
+    'create_boards',
+    'house_collection_to_string',
+    'BoardSection'
 ]
