d632e86c4b4ca5f189e894423436c925475429f2
omidekz <omidekz@gmail.com>
2021-02-08 10:32:19 +0330

    fix init file

--- algorithms/__init__.py
+++ algorithms/__init__.py
@@ -1,6 +1,6 @@
-from .boardstates import create_boards
-from .boardutils import get_board_section, Section as BoardSection
 from .house import house_collection_to_string
+from .boardutils import get_board_section, Section as BoardSection
+from .boardstates import create_boards, create_all_boards
 
 __all__ = [
     'create_boards',
