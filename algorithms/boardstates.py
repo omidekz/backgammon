42f8c94bf416ab6ae21f79e52127f861ce14ef54
omidekz <omidekz@gmail.com>
2021-02-09 20:15:36 +0330

    replace set with list for inc perf and duplicate states

--- algorithms/boardstates.py
+++ algorithms/boardstates.py
@@ -6,13 +6,13 @@ from enum import Enum
 
 
 def create_boards(board: Board, moves: int, marble: Marble) -> Sequence[Board]:
-    boards = list()
+    boards = set()
     houses_index = board.marble_houses(marble, indexes_seq=True)
     for index in houses_index:
         if board.can_move(index, marble.get_destination(index, moves), marble):
             new_board = board.copy()
             new_board.move_by(index, marble, moves)
-            boards.append(new_board)
+            boards.add(new_board)
     return boards
 
 def create_all_boards(
@@ -20,12 +20,14 @@ def create_all_boards(
     dices: Sequence[int],
     marble: Marble
 ) -> Sequence[Board]:
-    all_boards = []
+    all_boards = set()
     for dice in dices:
-        new_boards = []
+        new_boards = set()
         for board in boards:
-            new_boards.extend(create_boards(board, dice, marble))
+            bs = create_boards(board, dice, marble)
+            [new_boards.add(b) for b in bs]
         next_dices = dices.copy()
         next_dices.pop(next_dices.index(dice))
-        all_boards.extend(create_all_boards(new_boards, next_dices, marble))
+        bs = create_all_boards(new_boards, next_dices, marble)
+        [all_boards.add(b) for b in bs]
     return all_boards or boards
