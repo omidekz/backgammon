276a1d5ebebbf0e8cc48290fa564a8ee27f25ea9
omidekz <omidekz@gmail.com>
2021-02-09 22:20:16 +0330

    add more readablity to code

--- algorithms/boardstates.py
+++ algorithms/boardstates.py
@@ -1,13 +1,22 @@
 from __future__ import annotations
 from models import Marble, House
 from models.board import Board
-from typing import Sequence, Union
+from typing import Sequence, Union, Tuple
 from enum import Enum
 
 class CustomSet(set):
     def extend(self, itemslist: Sequence):
         for item in itemslist:
             self.add(item)
+
+    @staticmethod
+    def extract_and_create(list_in_list: Sequence[Sequence]):
+        items = []
+        for list in list_in_list:
+            if isinstance(list, Sequence):
+                list = [list]
+            items.extend(*list)
+        return CustomSet(iter(items))
 
 
 def create_boards(board: Board, moves: int, marble: Marble) -> Sequence[Board]:
@@ -18,7 +27,20 @@ def create_boards(board: Board, moves: int, marble: Marble) -> Sequence[Board]:
             new_board = board.copy()
             new_board.move_by(index, marble, moves)
             boards.add(new_board)
-    return boards
+    return list(boards)
+
+def create_possible_boards_from_boards_by_dice(boards: Sequence[Board], moves: int, marble: Marble) -> Sequence[Board]:
+    new_boards = CustomSet()
+    for board in boards:
+        all_unique_boards_by_current_dice = create_boards(
+            board, moves, marble
+        )
+        new_boards.extend(all_unique_boards_by_current_dice)
+    return new_boards
+
+def pop_dice(dices: Sequence[int], index: int) -> Tuple[int, Sequence[int]]:
+    tmp_dices = dices.copy()
+    return tmp_dices.pop(index), tmp_dices
 
 def create_all_boards(
     boards: Sequence[Board],
@@ -26,11 +48,15 @@ def create_all_boards(
     marble: Marble
 ) -> Sequence[Board]:
     all_boards = CustomSet()
-    for dice in dices:
-        new_boards = CustomSet()
-        for board in boards:
-            new_boards.extend(create_boards(board, dice, marble))
-        next_dices = dices.copy()
-        next_dices.pop(next_dices.index(dice))
-        all_boards.extend(create_all_boards(new_boards, next_dices, marble))
+    for dice_index in range(len(dices)):
+        dice, rest_of_dices = pop_dice(dices, dice_index)
+        all_unique_boards_by_current_dice = create_possible_boards_from_boards_by_dice(
+            boards, dice, marble
+        )
+        all_unique_boards = create_all_boards(
+            all_unique_boards_by_current_dice,
+            rest_of_dices,
+            marble
+        )
+        all_boards.extend(all_unique_boards)
     return all_boards or boards
